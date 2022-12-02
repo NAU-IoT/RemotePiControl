b1c551c7be4153810bfc9ea82159427b1902a4b2
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2022-12-01 19:15:35 -0700

    Update RPRConfiguration.py

--- RPRConfiguration.py
+++ RPRConfiguration.py
@@ -4,3 +4,4 @@ broker = "YOUR BROKER IP/HOSTNAME" #broker name
 cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile
 certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile
 keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile
+timezone = 'US/Arizona' #timezone for timestamps, consult pytz list of timezones for acceptable timezones
