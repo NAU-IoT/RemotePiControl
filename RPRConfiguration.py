b2f7b02d88e5fcd7a93e1170d81fbe80efab8504
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2022-12-01 18:25:25 -0700

    Create RPRConfiguration.py

---
+++ RPRConfiguration.py
@@ -0,0 +1,6 @@
+resetpin = 4 #IO pin 4 not actually pin 4 (actually pin 7 on Pi4). Change to any GPIO pin
+topic = "YOUR TOPIC NAME" #topic name
+broker = "YOUR BROKER IP/HOSTNAME" #broker name
+cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile
+certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile
+keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile
