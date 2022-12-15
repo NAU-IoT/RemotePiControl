62a111d7c496f106d55b2c381e033d97ba845ab8
MichaelChestnut <mwc72@nau.edu>
2022-12-15 12:49:25 -0700

    Updating RemotePiReset Code

---
+++ RemotePiReset.py
@@ -0,0 +1,79 @@
+#! /usr/bin/python
+
+import logging
+import paho.mqtt.client as mqtt
+import lgpio
+from pythonping import ping
+from datetime import datetime
+import pytz
+import time
+import RPRConfiguration as config
+
+#enable logging
+logging.basicConfig(level=logging.DEBUG)
+
+#import variables from config file
+ResetPin = config.resetpin #establishes reset pin
+Topic = config.topic	#establishes topic
+SystemUnderTest = config.client	#establishes system under test
+Broker = config.broker #establishes broker
+CA_Certs = config.cacert
+Certfile = config.certfile
+Keyfile = config.keyfile
+Timezone = config.timezone
+
+h = lgpio.gpiochip_open(0)	#enable gpio
+lgpio.gpio_claim_output(h, ResetPin) #set reset pin as output
+
+#create client instance
+client = mqtt.Client()
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    # subscribe, which need to put into on_connect
+    # if reconnect after losing the connection with the broker, it will continu>
+    client.subscribe(Topic)
+
+#define the publish function
+def publish(self, topic, data, qos=1, retain=False):
+    """
+      Publish to a topic.
+    """
+    logging.info("Publishing to topic %s" % topic)
+    self.client.publish(topic, data, qos=qos, retain=retain)
+
+
+# the callback function, it will be triggered when receiving messages
+def on_message(client, userdata, msg):
+#    print(f"{msg.topic} {msg.payload}")
+    if msg.payload.decode() == "reset":
+        ts = datetime.now(pytz.timezone(Timezone))
+        tsString = str(ts)
+        string0 = "\nReset executed at: {}\n".format(tsString) #formats string with timestamp
+        logging.debug(string0) #print time stamp when reset occurs
+        lgpio.gpio_write(h, ResetPin, 1) #set reset pin high
+        string1 = "{} is being reset".format(SystemUnderTest) #formats string with hostname
+        logging.debug(string1) #prints string 1 with hostname
+        time.sleep(5) #wait 5 seconds
+        lgpio.gpio_write(h, ResetPin, 0) #set reset pin low
+        string2 = "{} has been reset".format(SystemUnderTest) #formats string with hostname
+        logging.debug(string2)
+        logging.debug(ping(SystemUnderTest, verbose=True, count = 12, interval = 3))
+        logging.debug("-"*100)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+# set the will message, when the Raspberry Pi is powered off, or the network is>
+client.will_set(Topic, b'{"status": "Off"}')
+
+#establish tls set for secure connection over port 8883
+#client.tls_set(ca_certs=CA_Certs,
+#               certfile=Certfile,
+#               keyfile=Keyfile)
+
+# create connection, the three parameters are broker address, broker port numbe>
+client.connect(Broker, 1883, 60) #IP is the common name on the server cert
+
+# set the network loop blocking, it will not actively end the program before ca>
+client.loop_forever()
