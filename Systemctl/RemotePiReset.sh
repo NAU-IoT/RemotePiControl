bd2b996d984fa86862ea3b2c54727bc190814b0f
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2022-12-28 14:35:29 -0700

    Update RemotePiReset.sh

--- Systemctl/RemotePiReset.sh
+++ Systemctl/RemotePiReset.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #cd /home/mwc72/RemotePiReset
-python3 /SOME/PATH/TO/RemotePiReset.py >> /SOME/PATH/TO/logs/RemotePiReset.log 2>&1
+python3 /SOME/PATH/TO/RemotePiReset.py >> /SOME/PATH/TO/logs/RemotePiReset-`date '+%Y%m%d'`.log 2>&1
