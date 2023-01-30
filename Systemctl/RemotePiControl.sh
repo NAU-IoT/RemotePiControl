e3faf4cbd878f389b98ceed36454de81e2958f29
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-01-29 17:42:35 -0700

    Update and rename RemotePiReset.sh to RemotePiControl.sh

---
+++ Systemctl/RemotePiControl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 /SOME/PATH/TO/RemotePiControl.py >> /SOME/PATH/TO/logs/RemotePiControl-`date '+%Y%m%d'`.log 2>&1
--- Systemctl/RemotePiReset.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 /SOME/PATH/TO/RemotePiReset.py >> /SOME/PATH/TO/logs/RemotePiReset-`date '+%Y%m%d'`.log 2>&1
