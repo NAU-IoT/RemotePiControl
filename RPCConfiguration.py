733d3bcf1d9b9e491166573baf484de3d6e4ba0e
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-02-18 15:57:29 -0700

    Update RPCConfiguration.py

--- RPCConfiguration.py
+++ RPCConfiguration.py
@@ -1,6 +1,6 @@
 topic = "YOUR TOPIC NAME" #topic name
-broker = "YOUR BROKER IP/HOSTNAME" #broker name or IP address
-client = "YOUR CLIENT IP/HOSTNAME" #client name or IP address
+broker = "YOUR BROKER IP/HOSTNAME" #broker DNS name or IP address
+client = "YOUR CLIENT IP/HOSTNAME" #client DNS name or IP address
 cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile
 certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile
 keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile
