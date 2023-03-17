d66ec08bd528735a3b02cc871a01237be8ed6d12
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-03-17 16:17:45 -0700

    Create RPCConfiguration.py

---
+++ rpc-docker/RPCConfiguration.py
@@ -0,0 +1,8 @@
+topic = "YOUR TOPIC NAME" #topic name
+port = YOUR_MQTT_PORT_NUMBER #port to be used by mqtt
+broker = "YOUR BROKER IP/HOSTNAME" #broker DNS name or IP address
+client = "YOUR CLIENT IP/HOSTNAME" #client DNS name or IP address
+cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile
+certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile
+keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile
+timezone = 'US/Arizona' #timezone for timestamps, consult pytz list of timezones for acceptable timezones
