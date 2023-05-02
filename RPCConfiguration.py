66588a016896b35dca9e93385cc7e1e6629675fb
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-05-02 16:55:16 -0700

    Update RPCConfiguration.py

--- RPCConfiguration.py
+++ RPCConfiguration.py
@@ -1,7 +1,7 @@
-topic = "YOUR TOPIC NAME" #topic name
+topic = "RemotePiControl" #topic name
 port = YOUR_MQTT_PORT_NUMBER #port to be used by mqtt
-broker = "YOUR BROKER IP/HOSTNAME" #broker DNS name or IP address
-client = "YOUR CLIENT IP/HOSTNAME" #client DNS name or IP address
+broker = "YOUR BROKER IP/HOSTNAME" #broker DNS name or IP address; IoT Team: use localhost, supervisor IP address, or supervisor DNS name 
+client = "YOUR CLIENT IP/HOSTNAME" #client DNS name or IP address; IoT Team: use IOBoard IP address or IOBoard DNS name
 cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile, leave as is if not using keys
 certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile, leave as is if not using keys
 keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile, leave as is if not using keys
