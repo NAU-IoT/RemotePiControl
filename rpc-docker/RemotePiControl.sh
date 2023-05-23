b60fdb09bf9955d512cc0c0e34e59b9ca9e44361
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-05-23 08:21:25 -0700

    Update RemotePiControl.sh

--- rpc-docker/RemotePiControl.sh
+++ rpc-docker/RemotePiControl.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# cd /code/python/myapp
+# Find the process ID of the running RemotePiControl script
+pid=$(pgrep -f RemotePiControl.py)
+
+# Kill the process if it is running
+if [ -n "$pid" ]; then
+  kill "$pid"
+fi
+
 python3 /RemotePiControl.py >> /Data/logs/RemotePiControl-`date '+%Y%m%d'`.log 2>&1
