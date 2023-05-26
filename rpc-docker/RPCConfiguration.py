828411bb1559e151f37421054832d1c6af4edd45
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-05-26 11:24:28 -0700

    Update RPCConfiguration.py
    added capability to process up to 3 loads

--- rpc-docker/RPCConfiguration.py
+++ rpc-docker/RPCConfiguration.py
@@ -1,7 +1,9 @@
 topic = "RemotePiControl" #topic name
 port = YOUR_MQTT_PORT_NUMBER #port to be used by mqtt; IoT Team: use port 31883
-broker = "YOUR BROKER IP/HOSTNAME" #broker DNS name or IP address; IoT Team: use localhost, supervisor IP address, or supervisor DNS name 
-client = "YOUR CLIENT IP/HOSTNAME" #client DNS name or IP address; IoT Team: use IOBoard IP address or IOBoard DNS name
+broker = "YOUR BROKER IP/HOSTNAME" #broker DNS resolution or IP address; IoT Team: use localhost, supervisor IP address, or supervisor DNS resolution 
+client1 = "YOUR CLIENT IP/HOSTNAME" #client DNS resolution or IP address; IoT Team: use IOBoard IP address or IOBoard DNS resolution
+client2 = "YOUR CLIENT IP/HOSTNAME" #client DNS resolution or IP address; IoT Team: use SSD IP address or SSD DNS resolution
+client3 = "YOUR CLIENT IP/HOSTNAME" #client DNS resolution or IP address; IoT Team: use jetson IP address or jetson DNS resolution
 cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile, leave as is if not using keys
 certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile, leave as is if not using keys
 keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile, leave as is if not using keys
