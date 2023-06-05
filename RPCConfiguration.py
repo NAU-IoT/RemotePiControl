f8ad9eec6e64eed86889fe1b7146f94fc5f206ba
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-06-05 12:21:11 -0700

    Delete RPCConfiguration.py

--- RPCConfiguration.py
+++
@@ -1,8 +0,0 @@
-topic = "RemotePiControl" #topic name
-port = YOUR_MQTT_PORT_NUMBER #port to be used by mqtt
-broker = "YOUR BROKER IP/HOSTNAME" #broker DNS name or IP address; IoT Team: use localhost, supervisor IP address, or supervisor DNS name 
-client = "YOUR CLIENT IP/HOSTNAME" #client DNS name or IP address; IoT Team: use IOBoard IP address or IOBoard DNS name
-cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile, leave as is if not using keys
-certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile, leave as is if not using keys
-keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile, leave as is if not using keys
-timezone = 'US/Arizona' #timezone for timestamps, consult pytz list of timezones for acceptable timezones
