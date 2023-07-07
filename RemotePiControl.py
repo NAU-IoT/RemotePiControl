d8388b9cb174af71ee8d6240f8faa18d8ee4eb95
Michael Chestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-07-07 10:04:15 -0700

    Update RemotePiControl.py

--- RemotePiControl.py
+++ RemotePiControl.py
@@ -208,7 +208,12 @@ def main():
     load_config()
     #enable logging
     logging.basicConfig(level=logging.DEBUG)
-    
+
+    # Globalize variables
+    global Relay1Pin
+    global Relay2Pin
+    global Relay3Pin
+   
     Relay1Pin = 26  #initialize Relay1Pin to GPIO 26, which is actually pin 37, if using standalone relay, hook jumper up to this pin
     Relay2Pin = 20  #initialize Relay2Pin to GPIO 20, which is actually pin 38, if using standalone relay, hook jumper up to this pin
     Relay3Pin = 21  #initialize Relay3Pin to GPIO 21, which is actually pin 40, if using standalone relay, hook jumper up to this pin
